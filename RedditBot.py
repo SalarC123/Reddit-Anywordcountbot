1487608f967c47dd253cda63b65f615d343d42a5
Tabinda <Tabinda@chaudrys-iMac.local>
2020-07-25 21:11:28 -0500

    Fixed problem of code not replying by importing re (code would skip all errors because of the try-except statement before change)

--- RedditBot.py
+++ RedditBot.py
@@ -1,5 +1,7 @@
 import praw
+import re
 from botsecrets import PUT_CLIENT_ID_HERE, PUT_CLIENT_SECRET_HERE, PUT_PASSWORD_HERE, PUT_USER_AGENT_HERE, PUT_USER_NAME_HERE
+import time
 
 reddit = praw.Reddit(client_id= PUT_CLIENT_ID_HERE,
                      client_secret= PUT_CLIENT_SECRET_HERE,
@@ -7,40 +9,43 @@ reddit = praw.Reddit(client_id= PUT_CLIENT_ID_HERE,
                      user_agent= PUT_USER_AGENT_HERE,
                      username= PUT_USER_NAME_HERE)
 
+while True:
+    # Checks each comment in the generated stream of new comments
+    # for comment in reddit.inbox.unread(limit=None):
+    for comment in reddit.subreddit('all').stream.comments(skip_existing = True):
+        try:
+            # Checks for bot-name, username, word-to-check
+            text = re.compile(r'anywordcountbot(\s)+(\w)+(\s)+(\w)+',re.I).search(comment.body).group()
+        except:
+            continue
+        wordcount = 0
+        # Divides comment into the bot-name, username, word-to-check
+        commentattributes = text.split()
+        # Checks each submission title and increases count for every instance
+        for submission in reddit.redditor(commentattributes[1]).submissions.new(limit=None):
+            if commentattributes[2] in submission.title:
+                for word in submission.title.split():
+                    if commentattributes[2] in word:
+                        wordcount += 1
+            # Checks each submission text to increase count
+            if commentattributes[2] in submission.selftext:
+                for word in submission.selftext.split():
+                    if commentattributes[2] in word:
+                        wordcount += 1
+        # Checks each comment and increases count for every instance
+        for usercomment in reddit.redditor(commentattributes[1]).comments.new(limit=None):
+            if commentattributes[2] in usercomment.body:
+                for word in usercomment.body.split():
+                    if commentattributes[2] in word:
+                        wordcount += 1
+        # If the wordcount is one, times will not be plural ↓
+        times = 'times'
+        if wordcount == 1:
+            times = times[:4]
+        comment.reply(f'u/{commentattributes[1].title()} has said {commentattributes[2]} {wordcount} {times}')
+        # comment.mark_read()
+        time.sleep(2)
 
-# Checks each comment in the generated stream of new comments
-for comment in reddit.subreddit('all').stream.comments(skip_existing = True):
-    try:
-        # Checks for bot-name, username, word-to-check
-        text = re.compile(r'anywordcountbot(\s)+(\w)+(\s)+(\w)+',re.I).search(comment.body).group()
-    except:
-        continue
-    wordcount = 0
-    # Divides comment into the bot-name, username, word-to-check
-    commentattributes = text.split()
-    # Checks each submission title and increases count for every instance
-    for submission in reddit.redditor(commentattributes[1]).submissions.new(limit=None):
-        if commentattributes[2] in submission.title:
-            for word in submission.title.split():
-                if commentattributes[2] in word:
-                    wordcount += 1
-        # Checks each submission text to increase count
-        if commentattributes[2] in submission.selftext:
-            for word in submission.selftext.split():
-                if commentattributes[2] in word:
-                    wordcount += 1
-    # Checks each comment and increases count for every instance
-    for usercomment in reddit.redditor(commentattributes[1]).comments.new(limit=None):
-        if commentattributes[2] in usercomment.body:
-            for word in usercomment.body.split():
-                if commentattributes[2] in word:
-                    wordcount += 1
-    # If the wordcount is one, times will not be plural ↓
-    times = 'times'
-    if wordcount == 1:
-        times = times[:4]
-    comment.reply(f'u/{commentattributes[1].title()} has said {commentattributes[2]} {wordcount} {times}')
-    print('Posted')
 
 # ---- REFACTORING ----
 #
